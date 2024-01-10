6f6670320f7798403c1943626c2bf18a37ff440d
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2024-01-11 01:10:29 +0530

    Update maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedprefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/blur', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://raw.githubusercontent.com/Ayushdeveloper001/KawaiiMedia/main/95b119d40310f68a57927b564c8bbd46.jpg'),
     }), 'hornycard.png', '*[ ✔ ]*', m)
     }
     handler.help = ['blur','difuminar2']
