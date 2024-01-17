95ac25744f24ba0b3b77db3a8d60e46dcde8984e
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2024-01-17 22:12:19 +0530

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -126,17 +126,6 @@ case 'autobio':
       }
       chat.antiLink = isEnable
       break
-      
-      
-      case 'nsfw':
-      case '+18':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-            throw false
-           }}
-    chat.nsfw = isEnable          
-    break
 
     case 'autolevelup':
     isUser = true
